5d4f350886b3cabb7003643972d51db991a260d6
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-25 21:39:52 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -295,7 +295,7 @@ function install_ovpn(){
 ### Pasang SlowDNS
 function install_slowdns(){
     print_install "Memasang modul SlowDNS Server"
-    wget -q -O /tmp/nameserver "${REPO}slowdns/nameserver" >/dev/null 2>&1
+    wget -q -O /tmp/nameserver "${REPO}slowdns2/install-sldns" >/dev/null 2&1
     chmod +x /tmp/nameserver
     bash /tmp/nameserver | tee /root/install.log
     print_success "SlowDNS"
